0ff164bce8163309b46a3cb49c45d1b1373d28f7
kurakiyuka <myzlovebqb@sina.com>
2014-07-22 20:21:01 +0800

    7.22

--- SNHTickets/Panels/BuyTaskSetting.Designer.cs
+++ SNHTickets/Panels/BuyTaskSetting.Designer.cs
@@ -230,7 +230,12 @@
             "1",
             "2",
             "3",
+            "4",
+            "5",
+            "6",
             "7",
+            "8",
+            "9",
             "10",
             "20"});
             this.cb_onetime.Location = new System.Drawing.Point(95, 148);
